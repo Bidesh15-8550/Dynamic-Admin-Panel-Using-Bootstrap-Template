741105b48e16dfe5153e7e24f0bc26feb307519a
Bidesh15-8550 <bideshbiswas@ymail.com>
2020-03-13 13:38:41 +0600

    login page updated

--- login.php
+++ login.php
@@ -3,36 +3,50 @@
 session_start();
 
 ?>
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Login</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="style.css">
-    
-    <?php
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <link rel="shortcut icon" href="ico/favicon.png" />
+    <title>Admin Login</title>
+    <style>
+        body,html {
+    background-image: url('https://i.imgur.com/xhiRfL6.jpg');
+    height: 100%;
+}
+
+#profile-img {
+    height:180px;
+}
+.h-80 {
+    height: 80% !important;
+}
+    </style>
+       <?php
         include 'links.php'
     ?>
 </head>
 <body>
-    <header>
-        <div class="container center-div shadow ">
-            <div class="heading text-center text-uppercase text-white mb-3">Admin Login Page</div>
-            <div class="container row d-flex flex-row justify-content-center mb-3">
-                    <div class="admin-form shadow p-2">
+   
+        <div class="container h-80">
+         
+        <div class="row align-items-center h-100">
+            <div class="col-3 mx-auto">
+                <div class="text-center">
+                    <img id="profile-img" class="rounded-circle profile-img-card" src="https://i.imgur.com/6b6psnA.png" />
+                    <p id="profile-name" class="profile-name-card"></p>
                         <form action="logincheck.php" method="POST">
                             <div class="form-group">
-                                <label>Username</label>
+                                <label style="color:#FFFFFF;">Username</label>
                                 <input type="text" name="username"value="" class="form-control" autocomplete="off">
                             </div>
                             <div class="form-group">
-                                <label>Password</label>
+                                <label style="color:#FFFFFF;">Password</label>
                                 <input type="password" name="password"value="" class="form-control" autocomplete="off">
                             </div>
-                            <input type="submit" class="btn btn-success" name="submit">
+                            <input type="submit" value="Login" class="btn btn-success" name="submit">
                         </form>
                     
                     </div>
@@ -40,6 +54,6 @@ session_start();
             </div>
         
         </div>
-    </header>
+   
 </body>
 </html>
